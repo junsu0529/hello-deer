98e6e920494c0b84a39a04a4f3fce6836487d494
junsu <junsu0529@gmail.com>
2020-09-21 20:20:05 +0900

    구조 변경

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-<<<<<<< HEAD
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
-var MONGO_DB = 'mongodb://junsuAdmin:wnstn05@cluster0-shard-00-00.oyj21.mongodb.net:27017,cluster0-shard-00-01.oyj21.mongodb.net:27017,cluster0-shard-00-02.oyj21.mongodb.net:27017/Cluster0?ssl=true&replicaSet=atlas-5qn12e-shard-0&authSource=admin&retryWrites=true&w=majority';
+var MONGO_DB = 'mongodb+srv://junsuAdmin:wnstn05@deercluster.o9yl3.mongodb.net/DeerCluster?retryWrites=true&w=majority';
 
 //DB setting
 mongoose.set('useNewUrlParser', true);
@@ -11,24 +12,25 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect(MONGO_DB);
 var db = mongoose.connection;
-
 db.once('open', function(){
   console.log('DB connected');
 });
-
 db.on('error', function(err){
   console.log('DB ERROR : ',err);
 });
 
+// Other settings
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
+// Routes
+app.use('/', require('./routes/home'));
 
-
+// Port setting
 var port = 3000;
 app.listen(port, function(){ //3000번 포트에 node.js 서버 연결
   console.log('server on! http://localhost:'+port);
 });
-=======
-55555
->>>>>>> cae1b46934f38a684e16a524861492e65bc3224b
