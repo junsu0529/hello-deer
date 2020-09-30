b3300ef2683440de5a9d513dcafd451cd43105e7
070kyj <070kyj@naver.com>
2020-09-30 19:51:42 +0900

    post error added

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var router = express.Router();
 var User = require('../models/User');
+var util = require('../util'); // 1
+
 
 // Index // 1
 router.get('/', function(req, res){
@@ -26,7 +28,7 @@ router.post('/', function(req, res){
   User.create(req.body, function(err, user){
     if(err){
       req.flash('user', req.body);
-      req.flash('errors', parseError(err));
+      req.flash('errors', util.parseError(err));
       return res.redirect('/users/new');
     }
     res.redirect('/users');
@@ -74,7 +76,7 @@ router.put('/:username', function(req, res, next){
       user.save(function(err, user){
         if(err){
           req.flash('user', req.body);
-          req.flash('errors', parseError(err));
+          req.flash('errors', util.parseError(err));
           return res.redirect('/users/'+req.params.username+'/edit');
         }
         res.redirect('/users/'+user.username);
